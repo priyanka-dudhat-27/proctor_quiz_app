e12492a5f538e85540622e473098251c747835e1
priyanka-swiftrut <priyanka.dudhat@swiftrut.com>
2025-02-23 20:30:11 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+    }
+));
 
 // Routes
 app.use("/api/auth", authRoutes);
